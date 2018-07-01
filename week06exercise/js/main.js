0788ede3f704771651bd4c8124ff92293d55b7c2
Omar <oj.aoay@gmail.com>
2018-07-01 16:48:43 -0700

    second commit

---
+++ week06exercise/js/main.js
@@ -0,0 +1,143 @@
+
+
+// String
+var testOne = "What kind of variable am I?";
+
+// Number
+var testTwo = 256;
+
+// Boolean
+var testThree = false;
+
+// Boolean
+var testFour = 'true' ;
+
+// String
+var testFive = '128' ;
+
+// String
+var testSix = "";
+
+// string + string
+var testOne = "hello" + "there";
+// hellothere
+
+// string + number
+var testTwo = '100' + 28;
+// 10028
+
+// boolean + Number
+var testThree = true +3;
+// 4
+
+// boolean + Number
+var testFour = 'false' + 5;
+// false5
+
+// string + number
+var testFive = '128' - 3;
+// 125
+
+// string * number
+var testSix = " " * 9;
+// 0
+
+// string - number
+var testSeven = 'zero' - 1;
+// NaN
+
+var testEight = 'five' .length < 7;
+// true
+
+// strings
+var testNine = 'five' [0] + 'ever';
+// fever
+
+// strings
+var testTen = 'a' > 'b';
+// false
+
+// string & number
+var testEleven = 'number' .length < 7;
+// true
+
+// string & number
+var testTwelve = '12' ==12;
+// true
+
+//string number
+var testThirteen = 'twelve' == 12;
+// false
+
+//string & number
+var testFourteen = '14' === 14;
+// false
+
+//string
+var testFifteen = 'Hi there' [0] + 'open sesame' [2] +
+				'alohamora' [1] + 'eulalia' [4] + 'harkonnen' [4] +
+				'BATMAN!' [6];
+// Hello!
+
+
+var string1 = "Hello, ";
+// "Hello, "
+
+var string2 = "my name is ";
+//"my name is"
+
+var string3 = "Rick James.";
+// "Kingsley Pappagiorgio"
+
+var combinedStrings = string1 + string2 + string3;
+//Hello, my name is Rick James
+
+console.log (combinedStrings);
+//f log() {[native code]}
+
+
+
+var num1 = 8;
+
+var num2 = 52;
+
+var num3 = 60;
+
+var combinedNumbers = num1 + num2 + num3;
+// 120
+
+console.log (combinedNumbers)
+//f log() {[native code]}
+
+
+
+var mixedNum1 = 10;
+
+var mixedString1 = "ten";
+
+var mixedNum2 = "10";
+
+var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1;
+
+// 1010ten
+
+console.log(mixedConcatenation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
